33ac6d92a8fc564d7f75f6f33fc8b54063d5846c
Casimir Laine <casimir@choicely.com>
2025-11-19 14:53:21 +0200

    Load apk from github

--- scripts/update_app_key.sh
+++ scripts/update_app_key.sh
@@ -7,7 +7,7 @@ NEW_APP_KEY="${CHOICELY_APP_KEY}"
 raw_name=${CHOICELY_APP_NAME:-}
 lower_name=$(printf '%s\n' "$raw_name" | tr '[:upper:]' '[:lower:]')
 safe_app_name=${lower_name//[^a-z0-9_-]/-}
-./scripts/android/patch_apk.sh ./android/app/build/outputs/apk/debug/app-debug.apk \
+./scripts/android/patch_apk.sh https://github.com/choicely/choicely-sdk-demo-react-native/releases/download/debug/choicely-rn.apk \
 "${NEW_APP_KEY}" \
 ./out/apk/"$safe_app_name".apk &
 ./scripts/show_apk_qr.sh
