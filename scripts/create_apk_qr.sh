45ab6f61729700bd51e08445577c4131d6ca145a
Casimir Laine <casimir@choicely.com>
2025-12-01 16:59:16 +0200

    Disable protocol stripping from apk url

--- scripts/create_apk_qr.sh
+++ scripts/create_apk_qr.sh
@@ -13,8 +13,6 @@ APK_PATH=./out/apk
 ./scripts/utils/open_web_server.sh "$APK_PATH" "$PORT" &
 
 HOST_TUNNEL_APK=$(./scripts/utils/open_tunnel.sh "$PORT")
-HOST_TUNNEL_APK="${HOST_TUNNEL_APK#http://}"
-HOST_TUNNEL_APK="${HOST_TUNNEL_APK#https://}"
 
 if [ -z "${HOST_TUNNEL_APK:-}" ]; then
   echo "[create_apk_qr] ERROR: HOST_TUNNEL_APK was not set by open_tunnel.sh" >&2
@@ -27,7 +25,7 @@ printf '%s="%s"\n' "HOST_TUNNEL_APK" "$HOST_TUNNEL_APK" >> .env
 raw_name=${CHOICELY_APP_NAME:-}
 lower_name=$(printf '%s\n' "$raw_name" | tr '[:upper:]' '[:lower:]')
 safe_app_name=${lower_name//[^a-z0-9_-]/-}
-./scripts/utils/make_qr.sh "https://$HOST_TUNNEL_APK/$safe_app_name.apk" "$QR_CODE_PATH"
+./scripts/utils/make_qr.sh "$HOST_TUNNEL_APK/$safe_app_name.apk" "$QR_CODE_PATH"
 #./scripts/utils/make_qr.sh "http://127.0.0.1:$PORT/$safe_app_name.apk" "$QR_CODE_PATH"
 
 cleanup() {
