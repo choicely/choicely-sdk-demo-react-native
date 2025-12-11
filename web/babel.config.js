5cc36f734af4b5b96224603d393038621f91dc6b
Casimir <casimir@choicely.com>
2025-12-11 20:52:56 +0200

    Enable babel cache

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,8 +1,13 @@
 console.log('Loading babel.config.js...')
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-transform-modules-commonjs',
-  ],
+
+module.exports = function (api) {
+  api.cache(true)
+
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-transform-modules-commonjs',
+    ],
+  }
 }
