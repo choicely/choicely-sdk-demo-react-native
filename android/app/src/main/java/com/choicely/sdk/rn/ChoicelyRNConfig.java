757768ee0f68fa67a9df36930195d5d5936f2a33
Casimir <casimir@choicely.com>
2025-12-11 17:08:56 +0200

    Modularized react native config

---
+++ android/app/src/main/java/com/choicely/sdk/rn/ChoicelyRNConfig.java
@@ -0,0 +1,85 @@
+package com.choicely.sdk.rn;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.choicely.sdk.ChoicelySDK;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+
+public final class ChoicelyRNConfig {
+
+    private static final String PREFS_DEBUG_SERVER_HOST_KEY = "debug_http_host";
+    private static final String CHOICELY_CONFIG_FILE = "choicely_config.json";
+
+    private static SharedPreferences rnPrefs;
+
+    @NonNull
+    public static synchronized String loadConfigFromAssets(
+            @Nullable final String assetKey,
+            final int defaultResId,
+            @NonNull Context context
+    ) {
+        if (assetKey != null) {
+            try (InputStream is = context.getAssets().open(CHOICELY_CONFIG_FILE);
+                 final BufferedReader reader = new BufferedReader(
+                         new InputStreamReader(is, StandardCharsets.UTF_8))) {
+                final StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                final JSONObject json = new JSONObject(sb.toString());
+                final String appKey = json.getString(assetKey);
+                if (TextUtils.getTrimmedLength(appKey) > 0) {
+                    return appKey;
+                }
+            } catch (IOException | JSONException e) {
+            }
+        }
+        return context.getResources().getString(defaultResId);
+    }
+
+    public static synchronized void refreshRNConfig(@NonNull String appKey, @NonNull ChoicelyRNApplication app) {
+        ChoicelySDK.data().getChoicelyAppData(appKey)
+                .onData((appData) -> {
+                    if (appData == null) {
+                        return;
+                    }
+                    final JSONObject customData = appData.getCustomDataJson();
+                    if (customData == null) {
+                        return;
+                    }
+                    setRNHostDebug(customData.optString("bundle_url_mobile", ""), app);
+                }).onError((errorCode, message) -> {
+                }).getData();
+    }
+
+    public static synchronized void setRNHostDebug(@Nullable final String host, @NonNull ChoicelyRNApplication app) {
+        final boolean isDev = app.getReactNativeHost().getUseDeveloperSupport();
+        if (!isDev) {
+            return;
+        }
+        if (rnPrefs == null) {
+            rnPrefs = PreferenceManager.getDefaultSharedPreferences(app);
+        }
+        if (TextUtils.getTrimmedLength(host) > 0) {
+            rnPrefs.edit()
+                    .putString(PREFS_DEBUG_SERVER_HOST_KEY, host)
+                    .apply();
+        }
+    }
+}
--- android/app/src/main/java/com/choicely/sdk/rn/MyApplication.java
+++ android/app/src/main/java/com/choicely/sdk/rn/MyApplication.java
@@ -8,64 +8,39 @@ import com.choicely.sdk.ChoicelySDK;
 import com.choicely.sdk.rn.factory.MyContentFactory;
 import com.choicely.sdk.rn.factory.MySplashFactory;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+public class MyApplication extends ChoicelyRNApplication {
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
-public class MyApplication extends ChoicelyReactApplication {
-
-    private static final String CHOICELY_CONFIG_FILE = "choicely_config.json";
+    @NonNull
+    @Override
+    protected ChoicelyRNHost createReactNativeHost() {
+        return new ChoicelyRNHost(this) {
+            @NonNull
+            @Override
+            public String getJSMainModuleName() {
+                return "src/index";
+            }
+        };
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        this.setDebugHost(loadConfigFromAssets("rn_host_dev", R.string.rn_host_dev));
         this.initRNEngine();
-        final String appKey = loadConfigFromAssets("choicely_app_key", R.string.choicely_app_key);
-        initChoicely(appKey);
-        ChoicelySDK.data().getChoicelyAppData(appKey)
-                .onData((appData) -> {
-                    if (appData == null) {
-                        return;
-                    }
-                    final JSONObject customData = appData.getCustomDataJson();
-                    if (customData == null) {
-                        return;
-                    }
-                    this.setDebugHost(customData.optString("bundle_url_mobile", ""));
-                }).onError((errorCode, message) -> {
-                }).getData();
+        ChoicelyRNConfig.setRNHostDebug(ChoicelyRNConfig.loadConfigFromAssets("rn_host_dev", R.string.rn_host_dev, this), this);
+        final String appKey = ChoicelyRNConfig.loadConfigFromAssets("choicely_app_key", R.string.choicely_app_key, this);
+        if (TextUtils.getTrimmedLength(appKey) > 0) {
+            this.initChoicely(appKey);
+        }
     }
 
-    private void initChoicely(@NonNull final String appKey) {
-        // Core Choicely SDK bootstrapping with app key.
+    private synchronized void initChoicely(@NonNull final String appKey) {
+        // Core Choicely SDK bootstrapping with app key
         ChoicelySDK.init(this, appKey);
-        // Register custom factories to override default content + splash behavior.
+        // Register custom factories to override default content + splash behavior
         ChoicelySDK.factory().setContentFactory(new MyContentFactory());
         ChoicelySDK.factory().setSplashFactory(new MySplashFactory());
-    }
-
-    private String loadConfigFromAssets(@NonNull final String assetKey, final int defaultResId) {
-        try (InputStream is = this.getAssets().open(CHOICELY_CONFIG_FILE);
-             final BufferedReader reader = new BufferedReader(
-                     new InputStreamReader(is, StandardCharsets.UTF_8))) {
-            final StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-            final JSONObject json = new JSONObject(sb.toString());
-            final String appKey = json.getString(assetKey);
-            if (TextUtils.getTrimmedLength(appKey) > 0) {
-                return appKey;
-            }
-        } catch (IOException | JSONException e) {
-        }
-        return getResources().getString(defaultResId);
+        // Load Choicely React Native configuration.
+        ChoicelyRNConfig.refreshRNConfig(appKey, this);
     }
 }
