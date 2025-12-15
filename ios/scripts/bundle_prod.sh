c54a296cd7fb79abe348975b64568073e4c7f0cb
Casimir <casimir@choicely.com>
2025-12-15 18:10:22 +0200

    Fix release builds for ios

--- ios/scripts/bundle_prod.sh
+++ ios/scripts/bundle_prod.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+: "${REACT_NATIVE_PATH:?REACT_NATIVE_PATH is not defined (export REACT_NATIVE_PATH=/path/to/react-native)}"
+
 WITH_ENVIRONMENT="$REACT_NATIVE_PATH/scripts/xcode/with-environment.sh"
 REACT_NATIVE_XCODE="$REACT_NATIVE_PATH/scripts/react-native-xcode.sh"
+
+export ENTRY_FILE=${1:-src/index.js}
 
 /usr/bin/env bash -c "$WITH_ENVIRONMENT $REACT_NATIVE_XCODE"
